4b24768046ea76e2c01842b17eca65e1f65ed41a
Yanshun Guo <1315580108@qq.com>
2015-01-14 15:23:53 +0800

    Adds first demo which only use redis to write and read string type.

---
+++ SampleDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ServiceStack.Redis;
+
+namespace SampleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string host = "127.0.0.1";
+            const string testKeyName = "name";
+
+            using (var redisClient = new RedisClient(host))
+            {
+                if (!redisClient.ContainsKey(testKeyName))
+                {
+                    //Adds key and Sets value.
+                    redisClient.Set(testKeyName, "txdycmr");
+                }
+
+                Console.WriteLine("My name is: " + redisClient.Get<string>(testKeyName));
+                Console.WriteLine("Next I change my name into Yanshun Guo..");
+
+                //Changes the value of key named "name".
+                redisClient.Set(testKeyName, "Yanshun_Guo");
+                Console.WriteLine("Now my name is: " + redisClient.Get<string>(testKeyName));
+            }
+
+            Console.Read();
+        }
+    }
+}
